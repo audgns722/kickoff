05b220ab77b219d57e56aeea988ddcfb0b863cc3
audgns722 <144664895+audgns722@users.noreply.github.com>
2023-12-19 17:30:12 +0900

    231219다시

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,6 @@ import FindId from './components/user/FindId'
 import FindSuccess from './components/user/FindSuccess'
 import BoardList from './components/board/BoardList'
 import BoardDetail from './components/board/BoardDetail'
-import Join from './pages/Join'
 
 const App = () => {
   return (
