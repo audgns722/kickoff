2e2a3b4312d0493ee5118cfcb9680f7c4dbb4724
audgns722 <144664895+audgns722@users.noreply.github.com>
2023-12-20 14:11:39 +0900

    231220

--- client/src/components/user/Join.jsx
+++ client/src/components/user/Join.jsx
@@ -81,93 +81,95 @@ const JoinInfo = () => {
 
     return (
         <>
-            <div className="join__wrap" style={{ backgroundColor: "var(--white)" }}>
-                <div className="join__title">
-                    <h2>회원가입</h2>
-                </div>
-                <div className="join__index">
-                    <ul>
-                        <li></li>
-                        <li className="active2"></li>
-                        <li></li>
-                    </ul>
-                </div>
-                <div className="join__insert">
-                    <legend className="blind">회원가입 영역</legend>
-
-                    <div className="join">
-                        <label htmlFor="youEmail" className="required">이메일</label>
-                        <div className="write">
-                            <input
-                                type="email"
-                                id="youEmail"
-                                name="youEmail"
-                                placeholder="이메일을 적어주세요."
-                                className="input__style"
-                                required
-                                minLength={8}
-                                value={youEmail}
-                                onChange={(e) => setYouEmail(e.currentTarget.value)}
-                            />
-                            <button className="btn">중복체크</button>
-                            <p className="msg" id="youEmailComment">이메일은 8자 이상으로 입력해주세요. 영문,숫자만 사용 가능합니다.</p>
-                        </div>
+            <div style={{ backgroundColor: "var(--white)" }}>
+                <div className="join__wrap">
+                    <div className="join__title">
+                        <h2>회원가입</h2>
                     </div>
-
-                    <div className="join">
-                        <label htmlFor="youPass" className="required">비밀번호</label>
-                        <div className="write">
-                            <input
-                                type="password"
-                                id="youPass"
-                                name="youPass"
-                                placeholder="비밀번호를 적어주세요."
-                                className="input__style"
-                                autocomplete="off"
-                                required
-                                minLength={8}
-                                value={youPass}
-                                onChange={(e) => setYouPass(e.currentTarget.value)}
-                            />
-                            <p className="msg" id="youPassComment">비밀번호는 8자 이상으로 입력해주세요. 영문,숫자, 기호 사용 가능합니다.</p>
-                        </div>
+                    <div className="join__index">
+                        <ul>
+                            <li></li>
+                            <li className="active2"></li>
+                            <li></li>
+                        </ul>
                     </div>
+                    <div className="join__insert">
+                        <legend className="blind">회원가입 영역</legend>
 
-                    <div className="join">
-                        <label htmlFor="youPassC" className="required">비밀번호 확인</label>
-                        <div className="write">
-                            <input
-                                type="password"
-                                id="youPassC"
-                                name="youPassC"
-                                placeholder="다시 한번 비밀번호를 적어주세요."
-                                autocomplete="off"
-                                required
-                                className="input__style"
-                                value={youPassC}
-                                onChange={(e) => setYouPassC(e.currentTarget.value)}
-                            />
-                            <p className="msg" id="youPassCComment">비밀번호 확인을 위해 다시 한 번 입력해주세요</p>
+                        <div className="join">
+                            <label htmlFor="youEmail" className="required">이메일</label>
+                            <div className="write">
+                                <input
+                                    type="email"
+                                    id="youEmail"
+                                    name="youEmail"
+                                    placeholder="이메일을 적어주세요."
+                                    className="input__style"
+                                    required
+                                    minLength={8}
+                                    value={youEmail}
+                                    onChange={(e) => setYouEmail(e.currentTarget.value)}
+                                />
+                                <button className="btn">중복체크</button>
+                                <p className="msg" id="youEmailComment">이메일은 8자 이상으로 입력해주세요. 영문,숫자만 사용 가능합니다.</p>
+                            </div>
                         </div>
-                    </div>
-                    <div className="join">
-                        <label htmlFor="youName" className="required">이름</label>
-                        <div className="write">
-                            <input
-                                type="text"
-                                id="youName"
-                                name="youName"
-                                placeholder="이름을 적어주세요!"
-                                className="input__style"
-                                value={youName}
-                                onChange={(e) => setYouName(e.currentTarget.value)}
-                            />
-                            <p className="msg" id="youNameComment">정확한 이름을 넣어주세요. 문자만 사용 가능합니다.</p>
+
+                        <div className="join">
+                            <label htmlFor="youPass" className="required">비밀번호</label>
+                            <div className="write">
+                                <input
+                                    type="password"
+                                    id="youPass"
+                                    name="youPass"
+                                    placeholder="비밀번호를 적어주세요."
+                                    className="input__style"
+                                    autocomplete="off"
+                                    required
+                                    minLength={8}
+                                    value={youPass}
+                                    onChange={(e) => setYouPass(e.currentTarget.value)}
+                                />
+                                <p className="msg" id="youPassComment">비밀번호는 8자 이상으로 입력해주세요. 영문,숫자, 기호 사용 가능합니다.</p>
+                            </div>
                         </div>
+
+                        <div className="join">
+                            <label htmlFor="youPassC" className="required">비밀번호 확인</label>
+                            <div className="write">
+                                <input
+                                    type="password"
+                                    id="youPassC"
+                                    name="youPassC"
+                                    placeholder="다시 한번 비밀번호를 적어주세요."
+                                    autocomplete="off"
+                                    required
+                                    className="input__style"
+                                    value={youPassC}
+                                    onChange={(e) => setYouPassC(e.currentTarget.value)}
+                                />
+                                <p className="msg" id="youPassCComment">비밀번호 확인을 위해 다시 한 번 입력해주세요</p>
+                            </div>
+                        </div>
+                        <div className="join">
+                            <label htmlFor="youName" className="required">이름</label>
+                            <div className="write">
+                                <input
+                                    type="text"
+                                    id="youName"
+                                    name="youName"
+                                    placeholder="이름을 적어주세요!"
+                                    className="input__style"
+                                    value={youName}
+                                    onChange={(e) => setYouName(e.currentTarget.value)}
+                                />
+                                <p className="msg" id="youNameComment">정확한 이름을 넣어주세요. 문자만 사용 가능합니다.</p>
+                            </div>
+                        </div>
+
+                        <button disabled={flag} className="next" onClick={(e) => JoinFunc(e)}>회원가입 완료</button>
+
                     </div>
-
-                    <button disabled={flag} className="next" onClick={(e) => JoinFunc(e)}><Link to="/">회원가입 완료</Link></button>
-
                 </div>
             </div>
         </>
