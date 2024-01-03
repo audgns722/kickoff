99d0e70241293f6aabb8d1b3ce9c021b37edce8e
(YunJiSung) <(capitaljs@gmail.com)>
2024-01-03 17:46:44 +0900

    2024.01.03

--- client/src/components/main/News.jsx
+++ client/src/components/main/News.jsx
@@ -34,6 +34,71 @@
 // export default News;
 
 
+// import React, { useEffect, useState } from 'react';
+// import axios from 'axios';
+// import { Link } from 'react-router-dom';
+
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/autoplay';
+// import { Navigation } from 'swiper/modules';
+// import { Autoplay } from 'swiper/modules';
+
+
+
+// const News = () => {
+//     const [news, setNews] = useState([]);
+
+//     useEffect(() => {
+//         const fetchNews = async () => {
+//             try {
+//                 const response = await axios.post('/api/news');
+//                 console.log(response);
+//                 setNews(response.data.news.articles); // API의 응답 구조에 따라 조정하세요.
+//             } catch (error) {
+//                 console.error('뉴스를 불러오는데 실패했습니다.', error);
+//             }
+//         };
+
+//         fetchNews();
+//     }, []);
+
+//     return (
+//         <div className='main__news'>
+//             <h2>NEWS</h2>
+//             <Swiper
+//                 spaceBetween={15}
+//                 slidesPerView={'auto'}
+//                 navigation={true}
+//                 loop={true}
+//                 autoplay={{
+//                     delay: 3000,
+//                     disableOnInteraction: false,
+//                 }}
+//                 modules={[Autoplay, Navigation]}
+//                 className="mySwiper"
+//             >
+//                 {news && news.slice(0, 10).map((article, index) => (
+//                     <SwiperSlide key={index} className="news-slide">
+//                         <div className="news-content">
+//                             <p>
+//                                 <Link to={article.url} target='_blank' rel='noopener noreferrer'>
+//                                     {article.title}
+//                                 </Link>
+//                             </p>
+//                         </div>
+//                     </SwiperSlide>
+//                 ))}
+//             </Swiper>
+//         </div>
+//     );
+// };
+
+// export default News;
+
+
+
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
@@ -45,8 +110,6 @@ import 'swiper/css/autoplay';
 import { Navigation } from 'swiper/modules';
 import { Autoplay } from 'swiper/modules';
 
-
-
 const News = () => {
     const [news, setNews] = useState([]);
 
@@ -54,8 +117,9 @@ const News = () => {
         const fetchNews = async () => {
             try {
                 const response = await axios.post('/api/news');
-                console.log(response);
-                setNews(response.data.news.articles); // API의 응답 구조에 따라 조정하세요.
+                // console.log(response);
+                const sportNews = response.data;
+                setNews(sportNews);
             } catch (error) {
                 console.error('뉴스를 불러오는데 실패했습니다.', error);
             }
@@ -79,12 +143,12 @@ const News = () => {
                 modules={[Autoplay, Navigation]}
                 className="mySwiper"
             >
-                {news && news.slice(0, 10).map((article, index) => (
+                {news && news.slice(0, 10).map((item, index) => (
                     <SwiperSlide key={index} className="news-slide">
                         <div className="news-content">
                             <p>
-                                <Link to={article.url} target='_blank' rel='noopener noreferrer'>
-                                    {article.title}
+                                <Link to={item.link} target='_blank' rel='noopener noreferrer'>
+                                    {item.title.replace(/<b>|<\/b>/g, '')}
                                 </Link>
                             </p>
                         </div>
@@ -96,4 +160,3 @@ const News = () => {
 };
 
 export default News;
-
--- server/index.js
+++ server/index.js
@@ -135,6 +135,182 @@
 //     }
 // });
 
+// 가장 최근 사용
+// const mongoose = require("mongoose");
+// const express = require('express');
+// const axios = require('axios');
+// const cors = require('cors');
+
+// const path = require("path");
+// const app = express();
+// const port = "5050";
+// const config = require("./config/key.js");
+
+// // 모든 origin에서의 요청을 허용합니다. 실제 운영 환경에서는 특정 origin으로 제한하는 것이 좋습니다.
+// app.use(cors());
+
+// app.use(express.static(path.join(__dirname, "../client/build")));
+// app.use("/image", express.static("./image"));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// // express router
+// app.use("/api/board", require("./router/board.js"));
+// app.use("/api/user", require("./router/user.js"));
+// app.use("/api/reple", require("./router/reple.js"));
+
+// app.listen(port, () => {
+//     mongoose
+//         .connect(config.mongoURI)
+//         .then(() => {
+//             console.log("listening  --> " + port);
+//             console.log("mongoose --> connecting");
+//         })
+//         .catch((err) => {
+//             console.log(err)
+//         })
+// })
+
+// app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// })
+
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// })
+
+
+
+// // news
+// app.post('/api/news', async (req, res) => {
+//     try {
+//         const apiKey = config.apiKey;
+//         // const newsApiUrl = `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${apiKey}`;
+//         const newsApiUrl = `https://newsapi.org/v2/top-headlines?country=kr&category=sports&apiKey=${apiKey}`
+
+//         const response = await axios.get(newsApiUrl);
+//         res.status(200).json({ news: response.data });
+//     } catch (error) {
+//         console.error('Error fetching news:', error);
+//         res.status(500).json({ success: false });
+//     }
+// })
+
+// // 시즌 경기결과 최신순
+// app.post('/api/matches', async (req, res) => {
+//     const leagueNum = req.body.leagueNum;
+//     // const currentYear = new Date().getFullYear();
+//     const currentYear = 2023;
+
+//     try {
+//         const response = await axios.get(`http://api.football-data.org/v4/competitions/${leagueNum}/matches`, {
+//             headers: {
+//                 'X-Auth-Token': config.XAuthToken,
+//             },
+//             params: {
+//                 status: 'FINISHED',
+//                 season: currentYear.toString(),
+//             },
+//         });
+
+//         const reversedMatches = response.data.matches.reverse();
+//         res.json({ matches: reversedMatches });
+//     } catch (error) {
+//         console.error('Error fetching matches:', error);
+//         res.status(500).json({ success: false });
+//     }
+// });
+
+// // 시즌 예정된 경기
+// app.post('/api/scheduled', async (req, res) => {
+//     const leagueNum = req.body.leagueNum;
+//     // const currentYear = new Date().getFullYear();
+//     const currentYear = 2023;
+
+//     try {
+//         const response = await axios.get(`http://api.football-data.org/v4/competitions/${leagueNum}/matches`, {
+//             headers: {
+//                 'X-Auth-Token': config.XAuthToken,
+//             },
+//             params: {
+//                 status: 'SCHEDULED',
+//                 season: currentYear.toString(),
+//             },
+//         });
+
+//         const scheduledMatches = response.data.matches;
+//         res.json({ scheduled: scheduledMatches });
+//     } catch (error) {
+//         console.error('Error fetching scheduled matches:', error);
+//         res.status(500).json({ success: false });
+//     }
+// });
+
+// // league
+// app.post('/api/league', async (req, res) => {
+//     const leagueNum = req.body.leagueNum;
+
+//     try {
+//         const response = await axios.get(`http://api.football-data.org/v4/competitions/${leagueNum}/teams`, {
+//             headers: {
+//                 'X-Auth-Token': config.XAuthToken,
+//             }
+//         });
+//         res.status(200).json({ success: true, league: response.data });
+//     } catch (error) {
+//         console.error('Error fetching EPL data:', error);
+//         res.status(500).json({ success: false });
+//     }
+// });
+
+// // video
+// app.post('/api/video', async (req, res) => {
+//     const token = config.token;
+//     try {
+//         const response = await axios.get(`https://www.scorebat.com/video-api/v3/feed/?token=${token}`, {
+//         });
+//         res.json(response.data);
+//     } catch (error) {
+//         console.error('Error fetching matches:', error);
+//         res.status(500).json({ success: false });
+//     }
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const mongoose = require("mongoose");
 const express = require('express');
@@ -179,22 +355,44 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/build/index.html"));
 })
 
-
-
 // news
 app.post('/api/news', async (req, res) => {
     try {
-        const apiKey = config.apiKey;
-        // const newsApiUrl = `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${apiKey}`;
-        const newsApiUrl = `https://newsapi.org/v2/top-headlines?country=kr&category=sports&apiKey=${apiKey}`
-
-        const response = await axios.get(newsApiUrl);
-        res.status(200).json({ news: response.data });
+        const response = await axios.get('https://openapi.naver.com/v1/search/news.json', {
+            params: {
+                query: '&#xd574;&#xc678;&#xcd95;&#xad6c;',
+                display: "10",
+                start: "1",
+                sort: "sim"
+            },
+            headers: {
+                'X-Naver-Client-Id': config.NaverClientId,
+                'X-Naver-Client-Secret': config.NaverClientSecret
+            }
+        });
+        res.json(response.data.items);
     } catch (error) {
-        console.error('Error fetching news:', error);
-        res.status(500).json({ success: false });
+        res.status(500).send(error.toString());
     }
-})
+});
+
+// app.post('/api/news', async (req, res) => {
+//     try {
+//         const apiKey = config.apiKey;
+//         const countries = ['gb', 'de', 'fr', 'it', 'es'];
+//         const requests = countries.map(country =>
+//             axios.get(`https://newsapi.org/v2/top-headlines?country=${country}&category=sports&apiKey=${apiKey}`)
+//         );
+
+//         const responses = await Promise.all(requests);
+//         const newsData = responses.map(response => response.data);
+
+//         res.status(200).json({ news: newsData });
+//     } catch (error) {
+//         console.error('Error fetching news:', error);
+//         res.status(500).json({ success: false });
+//     }
+// });
 
 // 시즌 경기결과 최신순
 app.post('/api/matches', async (req, res) => {
